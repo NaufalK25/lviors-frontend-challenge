8b1a2ec28662ce2a5e1e145a0cd1be4d9a59a50e
NaufalK25 <naufalkateni2001@gmail.com>
2023-11-12 00:35:55 +0700

    refactor register page

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,54 +1,66 @@
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import AuthBtn from '../components/AuthBtn';
+import Input from '../components/Input';
 import FileInput from '../components/FileInput';
+import useGuard from '../hooks/useGuard';
+import { RegisterError, User } from '../types/auth';
 
 const RegisterPage = () => {
+  useGuard(true);
+
+  const navigate = useNavigate();
+
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<RegisterError>({});
+
   return (
-    <main className='flex justify-center items-center py-8'>
-      <form
-        action=''
-        className='w-[50vw] flex flex-col justify-center items-center border border-primary rounded p-10 gap-y-8'
-        encType='multipart/form-data'
-      >
-        <p className='text-xl'>Register</p>
-        <input
-          type='text'
-          placeholder='Name'
-          className='input'
-        />
-        <input
-          type='text'
-          placeholder='Username'
-          className='input'
-        />
-        <input
-          type='email'
-          placeholder='Email'
-          className='input'
-        />
-        <input
-          type='password'
-          placeholder='Password'
-          className='input'
-        />
-        <input
-          type='password'
-          placeholder='Confirm Password'
-          className='input'
-        />
-        <FileInput />
-        <div className='flex flex-col w-full justify-center items-center gap-y-4'>
-          <button className='w-2/3 bg-primary rounded-l-3xl rounded-r-3xl px-4 py-2 uppercase'>
-            Register
-          </button>
-          <Link
-            to='/login'
-            className='hover:underline text-primary'
-          >
-            Login
-          </Link>
-        </div>
-      </form>
-    </main>
+    <>
+      {loading && <div className='bg-primary h-2 loading'></div>}
+      <main className='flex justify-center items-center py-8'>
+        <form
+          className='w-[50vw] flex flex-col justify-center items-center border border-primary rounded p-10 gap-y-8'
+          encType='multipart/form-data'
+        >
+          <p className='text-xl'>Register</p>
+          <Input
+            error={error}
+            type='text'
+            field='name'
+          />
+          <Input
+            error={error}
+            type='text'
+            field='username'
+          />
+          <Input
+            error={error}
+            type='email'
+            field='email'
+          />
+          <Input
+            error={error}
+            type='password'
+            field='password'
+          />
+          <Input
+            error={error}
+            type='password'
+            field='confirm password'
+          />
+          <FileInput />
+          <div className='flex flex-col w-full justify-center items-center gap-y-4'>
+            <AuthBtn text='Register' />
+            <Link
+              to='/login'
+              className='hover:underline text-primary outline-primary p-1 rounded'
+            >
+              Login
+            </Link>
+          </div>
+        </form>
+      </main>
+    </>
   );
 };
 
